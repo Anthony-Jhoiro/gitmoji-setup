878679466dae45fe493847a116924fb6f5ac9858
anthony <quereantho@gmail.com>
2022-03-05 09:59:59 +0100

    :sparkles: setup releases

--- .releaserc.js
+++ .releaserc.js
@@ -2,13 +2,14 @@
 
 const dateFormat = require('dateformat')
 const { readFileSync } = require('fs');
+const path = require('path')
 
 const TEMPLATE_DIR = path.join(__dirname, '.github', 'templates')
 
 // Given a `const` variable `TEMPLATE_DIR` which points to "<semantic-release-gitmoji>/lib/assets/templates"
 
 // the *.hbs template and partials should be passed as strings of contents
-const template = readFileSync(path.join(TEMPLATE_DIR, 'default-template.hbs'))
+const template = readFileSync(path.join(TEMPLATE_DIR, 'release-note.hbs'))
 
 module.exports = {
     plugins: [
@@ -46,7 +47,18 @@ module.exports = {
                 }
             }
         ],
-        '@semantic-release/github',
-        '@semantic-release/npm'
+        '@semantic-release/github', [
+            '@semantic-release/git',
+            {
+                assets: ['CHANGELOG.md', 'package.json', 'README.md'],
+                message: ':bookmark: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+            },
+        ],
+        [
+            '@semantic-release/exec',
+            {
+                prepareCmd: 'yarn release',
+            },
+        ],
     ]
 }
